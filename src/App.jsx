06eacda5a0c42cce12ea353e2965e85eaa6eb579
AureliNabiel <justforfun170105@gmail.com>
2021-07-30 14:57:44 +0700

    Membuat Web Portofolio end

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import BackgroundImage from "./assets/bg.jpg";
 import FotoProfile from "./assets/pr.jpg";
+import ServiceImage1 from "./assets/si1.png";
+import ServiceImage2 from "./assets/si2.png";
+import ServiceImage3 from "./assets/si3.jpg";
+import WorkSection1 from "./assets/ws1.JPG";
+import WorkSection2 from "./assets/ws2.jpg";
+import WorkSection3 from "./assets/ws3.jpg";
+import WorkSection4 from "./assets/ws4.jpg";
 function App() {
   return (
     <React.Fragment>
@@ -47,7 +54,7 @@ function App() {
                 </p>
               </div>
               <img
-                className="w-64 h-64 rounded-full border-white shadow-xl flex-shrink-0"
+                className="w-64 h-64 rounded-full border-white shadow-xl flex-shrink-0 animate-pulse  "
                 src={FotoProfile}
                 alt=""
               />
@@ -55,6 +62,265 @@ function App() {
           </div>
         </div>
         {/* identity Card */}
+
+        {/* Services */}
+        <section id="services" className="bg-gray-50 pt-20 pb-28 px-8">
+          <div className="max-w-6xl mx-auto">
+            {/* Title */}
+            <div className="text-center">
+              <h1 className="text-6xl font-bold text-gray-700">Services</h1>
+              <p className="pt-2 text-xl">Heres What I Offer</p>
+            </div>
+            {/* Title */}
+            <div className="mt-24 grid grid-cols-3 gap-20">
+              {/* Services Inside Content 1 */}
+              <div className="relative">
+                <div className="absolute z-10 inset-0 bg-gradient-to-r from-blue-500 to bg-red-500 shadow-md rounded-lg transform skew-x-8 -rotate-6"></div>
+                <div className="relative z-20 bg-white h-full rounded-md shadow-md">
+                  <img
+                    className="rounded-t-md"
+                    style={{ height: "240px" }}
+                    src={ServiceImage1}
+                    alt="Service.jpg"
+                  />
+                  <div className="px-10 pb-6">
+                    <h2 className=" pt-3 font-bold text-2xl">
+                      Responsive Websites
+                    </h2>
+                    <p className="pt-2 font-semibold">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Aliquid numquam doloribus nesciunt maxime. Minus fuga
+                      nostrum magnam, ipsum nobis optio pariatur consequuntur
+                      quas exercitationem? At minima voluptate sint harum quis!
+                    </p>
+                  </div>
+                </div>
+              </div>
+              {/*Services Inside Content 1 */}
+
+              {/*Services Inside Content 2 */}
+              <div className="relative">
+                <div className="absolute z-10 inset-0 bg-gradient-to-r from-blue-500 to bg-red-500 shadow-md rounded-lg transform skew-x-8 -rotate-6"></div>
+                <div className="relative z-20 bg-white h-full rounded-md shadow-md">
+                  <img
+                    className="rounded-t-md bg-blue-200"
+                    style={{ height: "240px" }}
+                    src={ServiceImage2}
+                    alt="Service.jpg"
+                  />
+                  <div className="px-10 pb-6">
+                    <h2 className=" pt-3 font-bold text-2xl">
+                      Mobile Applications
+                    </h2>
+                    <p className="pt-2 font-semibold">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Aliquid numquam doloribus nesciunt maxime. Minus fuga
+                      nostrum magnam, ipsum nobis optio pariatur consequuntur
+                      quas exercitationem? At minima voluptate sint harum quis!
+                    </p>
+                  </div>
+                </div>
+              </div>
+              {/*Services Inside Content 2 */}
+
+              {/*Services Inside Content 3 */}
+              <div className="relative">
+                <div className="absolute z-10 inset-0 bg-gradient-to-r from-blue-500 to bg-red-500 shadow-md rounded-lg transform skew-x-8 -rotate-6"></div>
+                <div className="relative z-20 bg-white h-full rounded-md shadow-md">
+                  <img
+                    className="rounded-t-md"
+                    style={{ height: "240px" }}
+                    src={ServiceImage3}
+                    alt="Service.jpg"
+                  />
+                  <div className="px-10 pb-6">
+                    <h2 className=" pt-3 font-bold text-2xl">
+                      Mobile Applications
+                    </h2>
+                    <p className="pt-2 font-semibold">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Aliquid numquam doloribus nesciunt maxime. Minus fuga
+                      nostrum magnam, ipsum nobis optio pariatur consequuntur
+                      quas exercitationem? At minima voluptate sint harum quis!
+                    </p>
+                  </div>
+                </div>
+              </div>
+              {/* Services Inside Content 3 */}
+            </div>
+          </div>
+        </section>
+        {/* Services */}
+
+        {/* work */}
+        <section id="work" className="py-20 px-8">
+          <div className="max-w-6xl mx-auto">
+            <div className="text-center">
+              <h1 className="text-6xl font-bold text-gray-700">Work</h1>
+              <p className=" pt-3 text-xl">Here is My Portofolio</p>
+            </div>
+            <div className="mt-16 grid grid-cols-3 gap-14">
+              {/* Work Inside Content 1 */}
+              <div className="bg-white rounded-lg col-span-2">
+                <img
+                  className="object-cover w-full rounded-t-md h-80"
+                  src={WorkSection1}
+                  alt=""
+                />
+                <div className="p-8">
+                  <h3 className="text-2xl font-bold text-gray-800">
+                    Work Title
+                  </h3>
+                  <p className="pt-3 text-md font-bold">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Quaerat optio error placeat id perspiciatis nesciunt
+                    accusantium voluptatem suscipit labore. Omnis cupiditate eos
+                    quasi, dolore voluptate itaque recusandae id repudiandae
+                    exercitationem?
+                  </p>
+                  <a
+                    className="inline-block mt-4 px-6 py-2 text-white shadow-md rounded-lg bg-gradient-to-r from-blue-600 via-blue-500 to-blue-400"
+                    href=""
+                  >
+                    View More
+                  </a>
+                </div>
+              </div>
+              {/* Work Inside Content 1 */}
+
+              {/* Work Inside Content 2 */}
+              <div className="bg-white rounded-lg col-span-1">
+                <img
+                  className="object-cover w-full rounded-t-md h-80"
+                  src={WorkSection2}
+                  alt=""
+                />
+                <div className="p-8">
+                  <h3 className="text-2xl font-bold text-gray-800">
+                    Work Title
+                  </h3>
+                  <p className="pt-3 text-md font-bold">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Quaerat optio error placeat id perspiciatis nesciunt
+                    accusantium voluptatem suscipit labore. Omnis cupiditate eos
+                    quasi, dolore voluptate itaque recusandae id repudiandae
+                    exercitationem?
+                  </p>
+                  <a
+                    className="inline-block mt-4 px-6 py-2 text-white shadow-md rounded-lg bg-gradient-to-r from-blue-600 via-blue-500 to-blue-400"
+                    href=""
+                  >
+                    View More
+                  </a>
+                </div>
+              </div>
+              {/* Work Inside Content 2 */}
+
+              {/* Work Inside Content 3 */}
+              <div className="bg-white rounded-lg col-span-1">
+                <img
+                  className="object-cover w-full rounded-t-md h-80"
+                  src={WorkSection3}
+                  alt=""
+                />
+                <div className="p-8">
+                  <h3 className="text-2xl font-bold text-gray-800">
+                    Work Title
+                  </h3>
+                  <p className="pt-3 text-md font-bold">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Quaerat optio error placeat id perspiciatis nesciunt
+                    accusantium voluptatem suscipit labore. Omnis cupiditate eos
+                    quasi, dolore voluptate itaque recusandae id repudiandae
+                    exercitationem?
+                  </p>
+                  <a
+                    className="inline-block mt-4 px-6 py-2 text-white shadow-md rounded-lg bg-gradient-to-r from-blue-600 via-blue-500 to-blue-400"
+                    href=""
+                  >
+                    View More
+                  </a>
+                </div>
+              </div>
+              {/* Work Inside Content 3 */}
+
+              {/* Work Inside Content 4 */}
+              <div className="bg-white rounded-lg col-span-2">
+                <img
+                  className="object-cover w-full rounded-t-md h-80"
+                  src={WorkSection4}
+                  alt=""
+                />
+                <div className="p-8">
+                  <h3 className="text-2xl font-bold text-gray-800">
+                    Work Title
+                  </h3>
+                  <p className="pt-3 text-md font-bold">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Quaerat optio error placeat id perspiciatis nesciunt
+                    accusantium voluptatem suscipit labore. Omnis cupiditate eos
+                    quasi, dolore voluptate itaque recusandae id repudiandae
+                    exercitationem?
+                  </p>
+                  <a
+                    className="inline-block mt-4 px-6 py-2 text-white shadow-md rounded-lg bg-gradient-to-r from-blue-600 via-blue-500 to-blue-400"
+                    href=""
+                  >
+                    View More
+                  </a>
+                </div>
+              </div>
+              {/* Work Inside Content 4 */}
+            </div>
+          </div>
+        </section>
+        {/* work */}
+
+        {/* Contact */}
+        <section className="bg-gray-50 pt-20 pb-28 px-80 " id="contact">
+          <div className="max-w-6xl m-auto">
+            <div className="text-center">
+              <h1 className="text-6xl font-bold text-gray-700">Contact</h1>
+              <p className="pt-2 text-xl">Get in Touch Me</p>
+            </div>
+            <div className="mt-16 relative max-w-4xl mx-auto">
+              <div className="absolute z-10 inset-0 bg-gradient-to-r from-blue-500 to bg-red-500 shadow-md rounded-lg transform skew-x-8 rotate-6"></div>
+              <div className="relative z-20 bg-white rounded-md shadow-md p-12">
+                <form className="grid grid-cols-2 gap-x-8 gap-y-6" action="">
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    className="border border-gray-200 outline-none px-6 py-2 rounded-md hover:border-gray-400 focus:border-gray-400"
+                  />
+                  <input
+                    type="text"
+                    placeholder="Email"
+                    className="border border-gray-200 outline-none px-6 py-2 rounded-md hover:border-gray-400 focus:border-gray-400"
+                  />
+                  <input
+                    type="text"
+                    placeholder="Subject"
+                    className="col-span-2 border border-gray-200 outline-none px-6 py-2 rounded-md hover:border-gray-400 focus:border-gray-400"
+                  />
+                  <textarea
+                    className="col-span-2 border border-gray-200 outline-none px-6 py-2 rounded-md hover:border-gray-400 focus:border-gray-400"
+                    name="message"
+                    id="message"
+                    cols="30"
+                    rows="10"
+                  ></textarea>
+                  <div>
+                  <button className="inline-block mt-4 px-6 py-2 text-white shadow-md rounded-lg bg-gradient-to-r from-blue-600 via-blue-500 to-blue-400">
+                  Send message
+                  </button>
+                  </div>
+                 
+                </form>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* Contact */}
       </div>
     </React.Fragment>
   );
